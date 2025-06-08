a319d7a8c3a7c05e36d8b54585f8e5aabec07c12
GayathreeTata <147612438+GayathreeTata@users.noreply.github.com>
2025-06-08 13:50:26 +0530

    Create Employee Entity.java

---
+++ Employee Entity.java
@@ -0,0 +1,42 @@
+package com.example.employeemanagement.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees", 
+    uniqueConstraints = { 
+        @UniqueConstraint(columnNames = "email") 
+    })
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 50)
+    private String lastName;
+
+    @NotBlank
+    @Size(max = 100)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String department;
+
+    @NotBlank
+    @Size(max = 20)
+    private String position;
+
+    private double salary;
+}
